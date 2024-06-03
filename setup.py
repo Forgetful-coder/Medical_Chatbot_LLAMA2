9ccbc3177a5f489b277f4b5599e91020edd6ec92
Forgetful-coder <aayushaggarwal243@gmail.com>
2024-06-03 12:11:57 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Aayush Aggarwal',
+    author_email= 'aayushaggarwal243@gmail.com',
+    packages= find_packages(), #find __init__.py file 
+    install_requires = []
+
+)
